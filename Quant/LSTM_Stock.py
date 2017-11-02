7b79c4f3562223591a6a0e365894a3e25f1c25bf
georgezouq <zousongqi@foxmail.com>
2017-11-02 23:19:22 +0800

    Finish LSTM model

--- Quant/LSTM_Stock.py
+++ Quant/LSTM_Stock.py
@@ -3,6 +3,10 @@ import datetime
 import matplotlib.pyplot as plt
 import pandas_datareader.data as web
 from sklearn import preprocessing
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.recurrent import LSTM
+from keras.models import load_model
 
 stock_name = '^GSPC'
 seq_len = 22
@@ -33,3 +37,84 @@ def plot_stock(stock_name):
     plt.plot(df['Adj Close'], color='red', label='Adj Close')
     plt.legend(loc='best')
     plt.show()
+
+'''
+ Set last day Adjusted Close as y
+'''
+def load_data(stock, seq_len):
+    amount_of_features = len(stock.columns)
+    data = stock.as_matrix()
+    sequence_length = seq_len + 1 # index starting from 0
+    result = []
+
+    for index  in range(len(data) - sequence_length): # maxmimum date = lastest data - sequence length
+        result.append(data[index: index + sequence_length]) # index: index + 22days
+
+    result = np.array(result)
+    row = round(0.9 * result.shape[0]) # 90% split
+
+    train = result[:index(row), :] # 90% date
+    X_train = train[:, :-1][:, -1] # all data until data m
+    y_train = train[:, -1][:, -1] # day m + 1 adjusted close price
+
+    X_test = result[int(row):, :-1]
+    y_test = result[int(row):, -1][:, -1]
+
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], amount_of_features))
+    X_test = np.reshape(X_train, (X_test.shape[0], X_test.shape[1], amount_of_features))
+
+    return [X_train, y_train, X_test, y_test]
+
+X_train, y_train, X_test, y_test = load_data(df, seq_len)
+
+X_train.shape[0], X_train.shape[1], X_train.shape[2]
+
+y_train.shape[0]
+
+'''
+Build neural network
+'''
+
+def build_model2(layers, neurons, d):
+    model = Sequential()
+
+    model.add(LSTM(neurons[0], input_shape=(layers[1], return_sequences=True)))
+    model.add(Dropout(d))
+
+    model.add(LSTM(neurons[1], input_shape=(layers[1], return_sequences=False)))
+    model.add(Dropout(d))
+
+    model.add(Dense(neurons[2], kernel_initializer="uniform", activation="relu"))
+    model.add(Dense(neurons[3], kernel_initializer="uniform", activation="linear"))
+
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    return model
+
+'''
+Model Execution
+'''
+
+model = build_model2(shape, neurons, d)
+model.fit(
+    X_train,
+    y_train,
+    batch_size=512,
+    epochs=epochs,
+    validation_split=0.1,
+    verbose=1
+)
+
+'''
+Result on training set and testing set
+'''
+
+def model_score(model, X_train, y_train, X_test, y_test):
+    trainScore = model.evaluate(X_train, y_train, verbose=0)
+    print('Train Score: %.5f MSE (%.2f RMSE)' % (trainScore[0], math.sqrt(trainScore[0])))
+
+    testScore = model.evaluate(X_test, y_test, verbose=0)
+    print('Test Score: %.5f MSE (%.2f RMSE)' % (testScore[0], math.sqrt(testScore[0])))
+    return trainScore[0], testScore[0]
+
+model_score(model, X_train, y_train, X_test, y_test)
