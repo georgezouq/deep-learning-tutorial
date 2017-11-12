6742ea5de802402fa67d2c94dafaed94c880ba76
georgezouq <zousongqi@foxmail.com>
2017-11-12 21:52:46 +0800

    Implement pool layer forward

--- DeepLearning/CNN/main.py
+++ DeepLearning/CNN/main.py
@@ -42,3 +42,44 @@ def conv_forward(A_prev, W, b, hparameters):
     cache = (A_prev, W, b, hparameters)
 
     return Z, cache
+
+def pool_forward(A_prev, hparameters, mode="max"):
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    f = hparameters["f"]
+    stride = hparameters["stride"]
+
+    n_H = int(1 + (n_H_prev - f) / stride)
+    n_W = int(1 + (n_W_prev - f) / stride)
+    n_C = n_C_prev
+
+    A = np.zeros((m, n_H, n_W, n_C))
+
+    for i in range(m):  # loop over the training examples
+        for h in range(n_H):  # loop on the vertical axis of the output volume
+            for w in range(n_W):  # loop on the horizontal axis of the output volume
+                for c in range(n_C):  # loop over the channels of the output volume
+
+                    # Find the corners of the current "slice" (≈4 lines)
+                    vert_start = h * f
+                    vert_end = vert_start + f
+                    horiz_start = w * f
+                    horiz_end = horiz_start + f
+
+                    # Use the corners to define the current slice on the ith training example of A_prev, channel c. (≈1 line)
+                    a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, c]
+
+                    # Compute the pooling operation on the slice. Use an if statment to differentiate the modes. Use np.max/np.mean.
+                    if mode == "max":
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == "average":
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+    ### END CODE HERE ###
+
+    # Store the input and hparameters in "cache" for pool_backward()
+    cache = (A_prev, hparameters)
+
+    # Making sure your output shape is correct
+    assert (A.shape == (m, n_H, n_W, n_C))
+
+    return A, cache
