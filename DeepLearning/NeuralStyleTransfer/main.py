209941aaed96f72302ee1c69b2be9d1f73436d01
georgezouq <zousongqi@foxmail.com>
2017-12-13 23:23:52 +0800

    Build NST model

--- DeepLearning/NeuralStyleTransfer/main.py
+++ DeepLearning/NeuralStyleTransfer/main.py
@@ -77,3 +77,54 @@ def compute_style_cost(model, STYLE_LAYERS):
 def total_cost(J_content, J_style, alpha = 10, beta = 40):
     J = alpha * J_content + beta * J_style
     return J
+
+# 4. Solving the optimization problem
+
+tf.reset_default_graph()
+
+sess = tf.InteractiveSession()
+
+content_image = scipy.misc.imread("images/louvre_small.jpg")
+content_image = reshape_and_normalize_image(content_image)
+
+style_image = scipy.misc.imread("images/monet.jpg")
+style_image = reshape_and_normalize_image(style_image)
+
+generated_image = generate_noise_image(content_image)
+imshow(generated_image[0])
+
+model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
+
+sess.run(model['input'].assign(content_image))
+out = model['conv4_2']
+a_C = sess.run(out)
+a_G = out
+J_content = compute_content_cost(a_C, a_G)
+
+sess.run(model['input'].assign(style_image))
+J_style = compute_style_cost(model, STYLE_LAYERS)
+
+J = total_cost(J_content, J_style, alpha = 10, beta = 40)
+
+optimizer = tf.train.AdamOptimizer(2.0)
+train_step = optimizer.minimize(J)
+
+def model_nn(sess, input_image, num_iterations = 200):
+    sess.run(tf.global_variables_initializer())
+
+    sess.run(model['input'].assign(input_image))
+
+    for i in range(num_iterations):
+        sess.run(train_step)
+        generated_image = sess.run(model['input'])
+
+        if i % 20 == 0:
+            Jt, Jc, Js = sess.run([J, J_content, J_style])
+
+            save_image("output/" + str(i) + ".png", generated_image)
+
+        save_image('output/generated_image.jpg', generated_image)
+
+    return generated_image
+
+model_nn(sess, generated_image)
