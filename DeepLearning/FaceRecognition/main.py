bff91e90cdc5ed96915eca96c619e6a065e69e2d
georgezouq <zousongqi@foxmail.com>
2017-12-17 22:47:07 +0800

    Finish face recognition and it dependence

--- DeepLearning/FaceRecognition/main.py
+++ DeepLearning/FaceRecognition/main.py
@@ -26,4 +26,56 @@ np.set_printoptions(threshold=np.nan)
 
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
 
-FRmodel = faceRecoModel(input_shape=(3, 96, 96))
+def triplet_loss(y_true, y_pred, alpha=0.2):
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), keep_dims=True)
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), keep_dims=True)
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+
+    return loss
+
+database = {}
+database["danielle"] = img_to_encoding("images/danielle.png", FRmodel)
+database["younes"] = img_to_encoding("images/younes.jpg", FRmodel)
+database["tian"] = img_to_encoding("images/tian.jpg", FRmodel)
+database["andrew"] = img_to_encoding("images/andrew.jpg", FRmodel)
+database["kian"] = img_to_encoding("images/kian.jpg", FRmodel)
+database["dan"] = img_to_encoding("images/dan.jpg", FRmodel)
+database["sebastiano"] = img_to_encoding("images/sebastiano.jpg", FRmodel)
+database["bertrand"] = img_to_encoding("images/bertrand.jpg", FRmodel)
+database["kevin"] = img_to_encoding("images/kevin.jpg", FRmodel)
+database["felix"] = img_to_encoding("images/felix.jpg", FRmodel)
+database["benoit"] = img_to_encoding("images/benoit.jpg", FRmodel)
+database["arnaud"] = img_to_encoding("images/arnaud.jpg", FRmodel)
+
+def verify(image_path, identity, database, model):
+    encoding = img_to_encoding(image_path, model)
+    dist = np.linalg.norm(database[identity] - encoding)
+
+    if dist < 0.7:
+        print("It's " + str(identity) + ", welcome home!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False
+
+    return dist, door_open
+
+def who_is_it(image_path, database, model):
+    encoding = img_to_encoding(image_path, model)
+    min_dist = 100
+    for (name, db_enc) in database.items():
+        dist = np.linalg.norm(db_enc - encoding)
+
+        if dist < min_dist:
+            min_dist = dist
+            identity = name
+
+    if (min_dist > 0.7):
+        print("Not in the database.")
+    else:
+        print("It's" + str(identity) + ", the distance is " + str(min_dist))
+
+    return min_dist, identity
+
